bd0933d41f6cc7a91bcbfa3eecbbe41f4fba68a5
liuxiaofeng <liuxiaofeng@zhidemai.com>
2022-04-22 18:39:36 +0800

    新增个人练习module

---
+++ mall-exercise/src/main/java/com/liuxiaofeng/mall/exercise/MallExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.liuxiaofeng.mall.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallExerciseApplication.class, args);
+    }
+
+}
---
+++ mall-exercise/src/test/java/com/liuxiaofeng/mall/exercise/MallExerciseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.liuxiaofeng.mall.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
